b3fbb557d7a9341384e67c6eb1bf89075365c9de
Donald Kiprop <kipropdonald27@gmail.com>
2025-06-20 17:40:24 +0300

    Implement user authentication with password hashing using Bcrypt

--- server/app.py
+++ server/app.py
@@ -2,42 +2,78 @@
 
 from flask import request, session
 from flask_restful import Resource
-
 from config import app, db, api
 from models import User
 
-class ClearSession(Resource):
 
+class ClearSession(Resource):
     def delete(self):
-    
-        session['page_views'] = None
         session['user_id'] = None
+        return {}, 204
+
+
+class Signup(Resource):
+    def post(self):
+        json = request.get_json()
+
+        username = json.get('username')
+        password = json.get('password')
+
+        if not username or not password:
+            return {'error': 'Missing username or password'}, 422
+
+        user = User(username=username)
+        user.password_hash = password
+
+        db.session.add(user)
+        db.session.commit()
+
+        session['user_id'] = user.id
+
+        return user.to_dict(), 201
+
+
+class Login(Resource):
+    def post(self):
+        json = request.get_json()
+
+        username = json.get('username')
+        password = json.get('password')
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+
+        return {'error': 'Unauthorized'}, 401
+
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+
+        if user_id:
+            user = User.query.get(user_id)
+            if user:
+                return user.to_dict(), 200
 
         return {}, 204
 
-class Signup(Resource):
-    
-    def post(self):
-        json = request.get_json()
-        user = User(
-            username=json['username']
-        )
-        user.password_hash = json['password']
-        db.session.add(user)
-        db.session.commit()
-        return user.to_dict(), 201
 
-class CheckSession(Resource):
-    pass
-
-class Login(Resource):
-    pass
-
-class Logout(Resource):
-    pass
-
+# Register resources
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
